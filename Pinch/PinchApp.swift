194c0bfadef5550e8cda4c90927d9dd3948bd66e
Ricardo Sousa <r.sousa.888@gmail.com>
2024-02-20 12:56:13 +0000

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ricardo Sousa on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Ricardo Sousa on 20/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //: MARK: - PROPERTY
+    
+    //: MARK: - FUNCTION
+    
+    //: MARK: - CONTEN
+    var body: some View {
+        
+            Text("Hello, world!")
+            .padding()
+        }
+        
+    
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    ContentView()
+}
